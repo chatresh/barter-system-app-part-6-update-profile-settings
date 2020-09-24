0b8bb46aa268c1619a4ac5e800d83200d0908d5b
chatresh <60438260+chatresh@users.noreply.github.com>
2020-09-24 19:43:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { createAppContainer,createSwitchNavigator } from 'react-navigation';
+import { DrawerItems , createDrawerNavigator } from 'react-navigation-drawer';
+
+import customSideBarMenu from './components/customSideBarMenu'
+import { BottomNavigator } from './components/BottomNavigator';
+import  settings  from './components/settings';
+
+import LoginScreen from './screens/LoginScreen'
+
+export default function App() {
+  return (
+      <AppContainer/>
+  );
+}
+
+ const AppDrawerNavigator = createDrawerNavigator(
+ {
+   Home:{screen:BottomNavigator},
+   Settings:{screen:settings}
+ },
+ {
+        contentComponent: customSideBarMenu
+    },
+    {
+        initialRouteName : "Home"
+    }
+ )
+
+ const SwitchNavigator = createSwitchNavigator({
+     LoginScreen:LoginScreen,
+     AppDrawerNavigator:AppDrawerNavigator,
+   
+    
+ })
+
+ const AppContainer = createAppContainer(SwitchNavigator);
+
+
